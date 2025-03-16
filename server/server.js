19f469179495f04305159a56a265e8d3dadb0415
Prabal Bhandary <prabalbhandary97@gmail.com>
2025-03-16 16:45:29 +0545

    dir

--- server/server.js
+++ server/server.js
@@ -30,6 +30,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "./admin/dist")));
 
 //initializing routes
 app.use("/api/song", songRouter)
